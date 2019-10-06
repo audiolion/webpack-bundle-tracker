f429124dd16e690c5004fe14730b53d0b49e5cf5
Ryan Castner <castner.rr@gmail.com>
2019-10-06 09:12:52 -0400

    refactor to use hooks API in place of deprecated tapable api

--- index.js
+++ index.js
@@ -1,108 +1,129 @@
-var path = require('path');
-var fs = require('fs');
-var stripAnsi = require('strip-ansi');
-var mkdirp = require('mkdirp');
-var extend = require('deep-extend');
+var path = require("path");
+var fs = require("fs");
+var stripAnsi = require("strip-ansi");
+var mkdirp = require("mkdirp");
+var extend = require("deep-extend");
 
 var assets = {};
-var DEFAULT_OUTPUT_FILENAME = 'webpack-stats.json';
+var DEFAULT_OUTPUT_FILENAME = "webpack-stats.json";
 var DEFAULT_LOG_TIME = false;
-var DEFAULT_ASSET_CHUNK_NAME = 'assets';
-
+var DEFAULT_ASSET_CHUNK_NAME = "assets";
 
 function Plugin(options) {
   this.contents = {};
   this.options = options || {};
   this.options.filename = this.options.filename || DEFAULT_OUTPUT_FILENAME;
   this.options.trackAssets = this.options.trackAssets || false;
-  this.options.assetsChunkName = this.options.assetsChunkName || DEFAULT_ASSET_CHUNK_NAME;
+  this.options.assetsChunkName =
+    this.options.assetsChunkName || DEFAULT_ASSET_CHUNK_NAME;
   if (this.options.logTime === undefined) {
     this.options.logTime = DEFAULT_LOG_TIME;
   }
 }
 
 Plugin.prototype.apply = function(compiler) {
-    var self = this;
+  var self = this;
 
-    compiler.plugin('compilation', function(compilation, callback) {
-      compilation.plugin('failed-module', function(fail){
-        var output = {
-          status: 'error',
-          error: fail.error.name || 'unknown-error'
-        };
-        if (fail.error.module !== undefined) {
-          output.file = fail.error.module.userRequest;
-        }
-        if (fail.error.error !== undefined) {
-          output.message = stripAnsi(fail.error.error.codeFrame);
-        }
-        self.writeOutput(compiler, output);
-      });
-    });
-
-    compiler.plugin('compile', function(factory, callback) {
-      self.writeOutput(compiler, {status: 'compiling'});
-    });
-
-    compiler.plugin('done', function(stats){
-      if (stats.compilation.errors.length > 0) {
-        var error = stats.compilation.errors[0];
-        self.writeOutput(compiler, {
-          status: 'error',
-          error: error['name'] || 'unknown-error',
-          message: stripAnsi(error['message'])
-        });
-        return;
-      }
-
-      var chunks = {};
-      stats.compilation.chunks.map(function(chunk){
-        var files = chunk.files.map(function(file){
-          var F = {name: file};
-          if (compiler.options.output.publicPath) {
-            F.publicPath = compiler.options.output.publicPath + file;
-          }
-          if (compiler.options.output.path) {
-            F.path = path.join(compiler.options.output.path, file);
-          }
-          return F;
-        });
-        chunks[chunk.name] = files;
-      });
-
-      if (self.options.trackAssets === true) {
-        var assets = [];
-        Object.keys(stats.compilation.assets).map(function(asset){
-          var F = {name: asset};
-          if (compiler.options.output.publicPath) {
-            F.publicPath= compiler.options.output.publicPath + asset;
-          }
-          if (compiler.options.output.path) {
-            F.path = path.join(compiler.options.output.path, asset);
-          }
-          assets.push(F);
-        });
-        chunks[self.options.assetsChunkName] = assets;
-      }
-
+  const _compilation = function(compilation, callback) {
+    const failedModule = function(fail) {
       var output = {
-        status: 'done',
-        chunks: chunks
+        status: "error",
+        error: fail.error.name || "unknown-error"
       };
-
-      if (self.options.logTime === true) {
-        output.startTime = stats.startTime;
-        output.endTime = stats.endTime;
+      if (fail.error.module !== undefined) {
+        output.file = fail.error.module.userRequest;
       }
-
+      if (fail.error.module !== undefined) {
+        output.message = stripAnsi(fail.error.error.codeFrame);
+      } else {
+        output.message = "";
+      }
       self.writeOutput(compiler, output);
+    };
+
+    if (compilation.hooks) {
+      const plugin = { name: "BundleTrackerPlugin" };
+      compilation.hooks.failedModule.tap(plugin, failedModule);
+    } else {
+      compilation.plugin("failed-module", failedModule);
+    }
+  };
+
+  const compile = function(factory, callback) {
+    self.writeOutput(compiler, { status: "compiling" });
+  };
+
+  const done = function(stats) {
+    if (stats.compilation.errors.length > 0) {
+      var error = stats.compilation.errors[0];
+      self.writeOutput(compiler, {
+        status: "error",
+        error: error["name"] || "unknown-error",
+        message: stripAnsi(error["message"])
+      });
+      return;
+    }
+    var chunks = {};
+    stats.compilation.chunks.map(function(chunk) {
+      var files = chunk.files.map(function(file) {
+        var F = { name: file };
+        if (compiler.options.output.publicPath) {
+          F.publicPath = compiler.options.output.publicPath + file;
+        }
+        if (compiler.options.output.path) {
+          F.path = path.join(compiler.options.output.path, file);
+        }
+        return F;
+      });
+      chunks[chunk.name] = files;
     });
+
+    if (self.options.trackAssets === true) {
+      var assets = [];
+      Object.keys(stats.compilation.assets).map(function(asset) {
+        var F = { name: asset };
+        if (compiler.options.output.publicPath) {
+          F.publicPath = compiler.options.output.publicPath + asset;
+        }
+        if (compiler.options.output.path) {
+          F.path = path.join(compiler.options.output.path, asset);
+        }
+        assets.push(F);
+      });
+      chunks[self.options.assetsChunkName] = assets;
+    }
+
+    var output = {
+      status: "done",
+      chunks: chunks
+    };
+
+    if (self.options.logTime === true) {
+      output.startTime = stats.startTime;
+      output.endTime = stats.endTime;
+    }
+
+    self.writeOutput(compiler, output);
+  };
+
+  if (compiler.hooks) {
+    const plugin = { name: "BundleTrackerPlugin" };
+    compiler.hooks.compilation.tap(plugin, _compilation);
+    compiler.hooks.compile.tap(plugin, compile);
+    compiler.hooks.done.tap(plugin, done);
+  } else {
+    compiler.plugin("compilation", compilation);
+    compiler.plugin("compile", compile);
+    compiler.plugin("done", done);
+  }
 };
 
-
 Plugin.prototype.writeOutput = function(compiler, contents) {
-  var outputDir = this.options.path || '.';
-  var outputFilename = path.join(outputDir, this.options.filename || DEFAULT_OUTPUT_FILENAME);
+  var outputDir = this.options.path || ".";
+  var outputFilename = path.join(
+    outputDir,
+    this.options.filename || DEFAULT_OUTPUT_FILENAME
+  );
   if (compiler.options.output.publicPath) {
     contents.publicPath = compiler.options.output.publicPath;
   }
